5a98460bb2e75597b8e0e43abf8aa8965cf03799
Mircea-Gelu Egry <mirceaegry72@gmail.com>
2022-04-16 19:31:01 +0100

    profile view upadtes

--- ios-front/shopimize/shopimize/ShopperApp/Views/ProfileView.swift
+++ ios-front/shopimize/shopimize/ShopperApp/Views/ProfileView.swift
@@ -8,15 +8,132 @@
 import UIKit
 
 class ProfileView: UIView {
-
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isScrollEnabled = true
+        scrollView.bounces = false
+        scrollView.alwaysBounceVertical = true
+        return scrollView
+    }()
+    
+    let container: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = ViewConstants.mainStackSpacing
+        return stackView
+    }()
+    
+    let pageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Add store"
+        label.textColor = .systemIndigo
+        label.font = UIFont(name: ViewConstants.fontName, size: 30)
+        return label
+    }()
+    
+    // Name
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Store Name:"
+        label.font = UIFont(name: ViewConstants.fontName,
+                            size: ViewConstants.smallFontSize)
+        label.baselineAdjustment = .alignCenters
+        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return label
+    }()
+    
+    let name: CustomTextField = {
+        let textField = CustomTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.keyboardType = .default
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = .init(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.8)
+        textField.layer.cornerRadius = ViewConstants.formFieldCornerRadius
+        textField.setContentHuggingPriority(.defaultLow, for: .vertical)
+        return textField
+    }()
+    
+    let nameStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.spacing = ViewConstants.formFieldStackSpacing
+        return stack
+    }()
+    
+    
+    // MARK: Initializers
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .backgroundGrey
+        
+        setupViews()
+        setupConstraints()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("This class does not support NSCoder")
+    }
+    
+    // MARK: Class methods
+    
+    private func setupViews() {
+        self.addSubview(scrollView)
+        
+        scrollView.addSubview(container)
+        
+        container.addSubview(stackView)
+        
+        stackView.addArrangedSubview(pageLabel)
+        stackView.addArrangedSubview(nameStack)
+        
+        nameStack.addArrangedSubview(nameLabel)
+        nameStack.addArrangedSubview(name)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            
+            container.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            container.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
+            container.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
+            container.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            container.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: container.topAnchor),
+            stackView.rightAnchor.constraint(equalTo: container.rightAnchor, constant: -ViewConstants.formPadding),
+            stackView.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -ViewConstants.formBottomPadding),
+            stackView.leftAnchor.constraint(equalTo: container.leftAnchor, constant: ViewConstants.formPadding),
+            
+            nameStack.heightAnchor.constraint(equalToConstant: ViewConstants.formFieldHeight),
+            
+            
+            
+        ])
     }
     
 }
