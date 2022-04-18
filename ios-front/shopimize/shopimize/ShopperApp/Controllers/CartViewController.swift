3a75bc2ca45ca6d86fa6bfb8530286006624cb80
Mircea-Gelu Egry <mirceaegry72@gmail.com>
2022-04-18 08:48:03 +0100

    cartViewController updates

--- ios-front/shopimize/shopimize/ShopperApp/Controllers/CartViewController.swift
+++ ios-front/shopimize/shopimize/ShopperApp/Controllers/CartViewController.swift
@@ -10,19 +10,65 @@ import UIKit
 /// Controller for the shopping cart screen of the app
 
 class CartViewController: UIViewController {
-
-    let cartView = CartView()
     
-    override func loadView() {
-        view = cartView
-    }
+    let tableView = UITableView()
     
-    /// Do any aditional setup after the view loaded
-    ///
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .backgroundGrey
+        
+        setupNavigation()
+        setupTable()
+        setupConstraints()
     }
+    
+    private func setupTable() {
+        view.addSubview(tableView)
+        
+        tableView.backgroundColor = .backgroundGrey
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CartTableViewCell.self, forCellReuseIdentifier: "cartCell")
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupNavigation() {
+        navigationItem.title = "Your cart"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+}
 
+extension CartViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as? CartTableViewCell else {
+            fatalError("Can not dequeue cell with identifier friendsCell")
+        }
+        
+        cell.userLabel.text = "item 1"
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(TableConstants.friendsCellHeight)
+    }
 }
---
+++ ios-front/shopimize/shopimize/ShopperApp/Views/TableViewCells/CartTableViewCell.swift
@@ -0,0 +1,107 @@
+//
+//  CartTableViewCell.swift
+//  shopimize
+//
+//  Created by Mircea Egry on 18/04/2022.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    /// Container holding the card for the cell
+    var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = false
+        view.layer.shadowRadius = 4.0
+        view.layer.shadowOpacity = 0.4
+        view.layer.shadowColor = UIColor.gray.cgColor
+        view.layer.shadowOffset = CGSize(width: 2, height: 5)
+        return view
+    }()
+    
+    var image: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "person")
+        image.contentMode = .scaleAspectFit
+        image.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return image
+    }()
+    
+    var userLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.contentMode = .scaleAspectFit
+        label.textColor = .black
+        return label
+    }()
+    
+    var shoppingStatusLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.contentMode = .scaleAspectFit
+        label.textColor = .black
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .backgroundGrey
+        setupContainer()
+        setupConstraints()
+    }
+    
+    /// No support for storyboards so the initializer is set to unavailable
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    /// Setup the container with all its views
+    private func setupContainer() {
+        // self.contentView.backgroundColor = .clear
+        addSubview(containerView)
+        addSubview(image)
+        addSubview(userLabel)
+        addSubview(shoppingStatusLabel)
+    }
+    
+    /// Setup the views constraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            // Container constraints
+            
+            containerView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor,
+                                               constant: CGFloat(TableConstants.cellVerticalInset)),
+            
+            containerView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor,
+                                                 constant: -CGFloat(TableConstants.cellHorizontalInset)),
+            
+            containerView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor,
+                                                  constant: -CGFloat(TableConstants.cellVerticalInset)),
+            
+            containerView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor,
+                                                constant: CGFloat(TableConstants.cellHorizontalInset)),
+            
+            image.widthAnchor.constraint(equalToConstant: 80),
+            image.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5),
+            image.leftAnchor.constraint(equalTo: containerView.leftAnchor),
+            image.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            
+            userLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5),
+            userLabel.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 35),
+            userLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+            
+            shoppingStatusLabel.topAnchor.constraint(equalTo: userLabel.bottomAnchor),
+            shoppingStatusLabel.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 35),
+            shoppingStatusLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor),
+            shoppingStatusLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+    }
+
+}
